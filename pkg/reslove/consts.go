2953c7ec903860b6adbf01895070cd1730dbd33f
ClessLi <316600949@qq.com>
2020-10-12 13:56:33 +0800

    调整正则匹配规则

--- pkg/reslove/consts.go
+++ pkg/reslove/consts.go
@@ -1,7 +1,8 @@
 package reslove
 
 const (
-	ipv4AddressReg          = `(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})`
-	HostBruteLoginRecordReg = `^.*pam_unix\(sshd:auth\): authentication failure.*rhost=(\S*)\s+.*$`
+	ipv4AddressReg = `(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})`
+	//HostBruteLoginRecordReg = `^.*pam_unix\(sshd:auth\): authentication failure.*rhost=(\S*)\s+.*$`
+	HostBruteLoginRecordReg = `^.*pam_unix\(sshd:auth\): authentication failure.*rhost=(\S*)`
 	//HostBruteLoginRecordWithUserReg = `^.*pam_unix\(sshd:auth\): authentication failure.*rhost=(.*)\s+user=(\S*)\s*.*$`
 )
